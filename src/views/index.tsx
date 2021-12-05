969c7a805d43bcb4129f2fc39c0a2c4d3c2cdd2e
niks1860 <49323912+niks1860@users.noreply.github.com>
2021-12-05 12:24:42 +0530

    Setup layout with Material UI

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -6,10 +6,11 @@ const Views = () => {
     <Routes>
       <Route path="/auth" element={<div>Sign In / Sign Up Page</div>} />
       <Route element={<Layout />}>
-        <Route path="news" element={<div>News Page</div>} />
+        <Route path="stories" element={<div>Stories Page</div>} />
         <Route path="comments" element={<div>Comments Page</div>} />
         <Route path="asks" element={<div>Ask Page</div>} />
-        <Route path="" element={<Navigate to="/news" />} />
+        <Route path="jobs" element={<div>Jobs Page</div>} />
+        <Route path="" element={<Navigate to="/stories" />} />
       </Route>
     </Routes>
   )
--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -1,11 +1,145 @@
-import { Outlet } from "react-router-dom"
+import React, { useState } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  Button,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Divider,
+  Link,
+  Grid,
+} from "@mui/material"
+import { Menu as MenuIcon } from "@mui/icons-material"
+
+interface NavItem {
+  text: string
+  to: string
+}
+
+const menus: NavItem[] = [
+  { text: "Stories", to: "/stories" },
+  { text: "Comments", to: "/comments" },
+  { text: "Asks", to: "/asks" },
+  { text: "Jobs", to: "/jobs" },
+]
+
+const footerItems: NavItem[] = [
+  { text: "Guidelines", to: "https://news.ycombinator.com/newsguidelines.html" },
+  { text: "FAQ", to: "https://news.ycombinator.com/newsfaq.html" },
+  { text: "Lists", to: "https://news.ycombinator.com/lists" },
+  { text: "API", to: "https://github.com/HackerNews/API" },
+  { text: "Security", to: "https://news.ycombinator.com/security.html" },
+  { text: "Legal", to: "http://www.ycombinator.com/legal/" },
+  { text: "Apply to YC", to: "http://www.ycombinator.com/apply/" },
+]
 
 const Layout = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+  const navigate = useNavigate()
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  const navigateTo = (to?: string) => {
+    navigate(to || "")
+    handleCloseNavMenu()
+  }
+
   return (
     <div>
-      <div>Header</div>
-      <Outlet />
-      <div>Footer</div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", sm: "flex" } }}
+            >
+              Hacker News
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="Naigation links"
+                aria-control="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", sm: "none" },
+                }}
+              >
+                {menus.map(({ text, to }) => (
+                  <MenuItem key={to} onClick={() => navigateTo(to)}>
+                    <Typography textAlign="center">{text}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}
+            >
+              Hacker News
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}>
+              {menus.map(({ text, to }) => (
+                <Button
+                  key={to}
+                  onClick={() => navigateTo(to)}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {text}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box component="main" sx={{ p: 3 }}>
+        <Outlet />
+      </Box>
+      <Divider />
+      <Grid sx={{ p: 3 }} container spacing={3}>
+        {footerItems.map(({ text, to }) => (
+          <Grid item key={to}>
+            <Link target="_blank" underline="none" rel="noreferrer" href={to}>
+              {text}
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
